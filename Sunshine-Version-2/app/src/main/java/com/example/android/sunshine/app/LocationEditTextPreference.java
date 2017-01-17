1e1784cdbb61e803b67ee0c8801f1acaa44ab27e
lwonho92 <lwonho92@gmail.com>
2017-01-18 01:49:31 +0900

    Modify Custom EditTextView.
    Modify Custom EditTextView.

---
+++ Sunshine-Version-2/app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -0,0 +1,66 @@
+package com.example.android.sunshine.app;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by MY on 2017-01-17.
+ */
+
+public class LocationEditTextPreference extends EditTextPreference {
+    private static final int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
+    private int minLen;
+
+    public LocationEditTextPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.LocationEditTextPreference,
+                0, 0);
+        try {
+            minLen = typedArray.getInteger(R.styleable.LocationEditTextPreference_minLength, DEFAULT_MINIMUM_LOCATION_LENGTH);
+        } finally {
+            typedArray.recycle();
+        }
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+
+        EditText editText = getEditText();
+        editText.addTextChangedListener(new TextWatcher() {
+
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Dialog dialog = getDialog();
+                if (dialog instanceof AlertDialog) {
+                    AlertDialog alertDialog = (AlertDialog) dialog;
+                    Button positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    // Check if the EditText is empty
+                    if (editable.length() < minLen) {
+                        positiveButton.setEnabled(false);
+                    } else {
+                        positiveButton.setEnabled(true);
+                    }
+                }
+            }
+        });
+    }
+}
